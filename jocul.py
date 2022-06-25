b00ce6fbc26b094bb8967434fab13180b53d301d
cadfrunze <78301491+cadfrunze@users.noreply.github.com>
2022-06-25 14:39:44 +0300

    Update jocul.py

--- jocul.py
+++ jocul.py
@@ -1,10 +1,14 @@
 from numbers import Number
+from pickle import TRUE
 from art import logo
 import random
 import string
 print(logo)
 TABEL_CARACTERE = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', '!', '"', '#', '$', '%', '&', "'", '(', ')', '*', '+', ',', '-', '.', '/', ':', ';', '<', '=', '>', '?', '@', '[', '\\', ']', '^', '_', '`', '{', '|', '}', '~',]
-TABEL_NUMERE = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, "", " "]
+TABEL_NUMERE = [
+    '1', '2', '3', '4', '5', '6', '7', '8', '9', '10', '11', '12', '13', '14', '15', '16', '17', '18', '19', '20', '21', '22', '23', '24', '25', '26', '27', '28', '29', '30', '31', '32', '33', '34', '35', '36', '37', '38', '39', '40', '41', '42', '43', '44', '45', '46', '47', '48', '49', '50', '51', '52', '53', '54', '55', '56', '57', '58', '59', '60', '61', '62', '63', '64', '65', '66', '67', '68', '69', '70', '71', '72', '73', '74', 
+'75', '76', '77', '78', '79', '80', '81', '82', '83', '84', '85', '86', '87', '88', '89', '90', '91', '92', '93', '94', '95', '96', '97', '98', '99', '100', '101'
+]
 def numarul_ales():
     return random.randint(1, 100)
 NUMARUL_ALES = numarul_ales()
@@ -31,17 +35,25 @@ def jocul():
         elif len(alegere_numar) > 3:
             alegere_numar = input("Ai introdus prea multe caractere!. Alege un numar de la 1 la 100: ")
         for i in alegere_numar:
+            proba = 0
             if i in TABEL_CARACTERE:
                 alegere_numar = input(f"Hey! Vezi ca nu ai scris o cifra. Alege un numar de la 1 la 100: ")
                 game0 = False
-        for a in alegere_numar:
-            if a not in TABEL_CARACTERE:
-                game0 = True
+            elif i == " ":
+                alegere_numar = alegere_numar + i.replace(" ", "")
+                i = alegere_numar[-1]
+            if i in TABEL_NUMERE:
+                proba = proba + 1
+            if proba == len(alegere_numar):
+                while alegere_numar in numere_alese:
+                    alegere_numar = input(f"Hey! Vezi ca ai mai scris acest numar ({numere_alese[-1]}). Alege un alt numar: ")
+                    game0 = False
+                if alegere_numar in TABEL_NUMERE:
+                    game0 = True
+                    
         if game0 == True:
-            alegere_numar = int(alegere_numar)
-            while alegere_numar in numere_alese:
-                    alegere_numar = int(input(f"Hey! Vezi ca ai mai scris acest numar ({numere_alese[-1]}). Alege un alt numar: "))
             numere_alese.append(alegere_numar)
+            alegere_numar = int(alegere_numar)
             while alegere_numar > 100 or alegere_numar < 1:
                 if alegere_numar > 100:
                     alegere_numar = input(f"Hey! Vezi ca ai mai scris un numar mai mare ca si 100. Alege un numar de la 1 la 100: ")
